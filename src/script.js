c21ab604e72282324c8f396ec0809e394a5f80bc
Mateusz Bąk <bakmateusz@icloud.com>
2021-08-08 23:16:43 +0200

    Add easter egg

--- src/script.js
+++ src/script.js
@@ -41,7 +41,7 @@ async function start(){
     $('#text-container').classList.remove('hidden')
     
     // display result
-    setInformationText((result) ? 'the system has been bypassed.' : "The system didn't accept your answers")
+    setInformationText((result) ? 'No i łatwo, jedziesz z tym! Serduszka dla Kamilka czacik! 💜💜💜💜💜💜💜💜💜💜' : "The system didn't accept your answers")
     if(!result) $('.spy-icon').src = 'assets/failed.png'
 
     $('#answer-reveal').textContent = answer
